9220ea4bd52fe8a2c214c3cffbd9c1b3f20430fd
James Newton <redshodan@gmail.com>
2012-06-27 06:15:39 -0600

    Fleshed out the setup.py install stage. Added long help for both the manpage and --help-long.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: latin-1 -*-
 
 #
 # The ManPageFormatter and build_manpage were borrowed from:
@@ -12,16 +13,29 @@ from distutils.core import setup, Command
 
 import imp
 lssh = imp.load_source("lssh", "lssh")
-# Remove the compiled file
-os.unlink("lsshc")
+# Remove the compiled file, ignore failures
+try:
+    os.unlink("lsshc")
+except:
+    pass
 
+MANDIR = "man/man1"
+BUILD_MANDIR = os.path.join("build", MANDIR)
+BUILD_MANPAGE = os.path.join(BUILD_MANDIR, "lssh.1")
+COPYRIGHT = \
+  ("Copyright © 2012 Chris Newton. License GPLv2: GNU GPL version 2 " +
+  "<http://gnu.org/licenses/gpl.html>. This is free software: you are free " +
+  "to change and redistribute it.  There is NO WARRANTY, to the extent " +
+  "permitted by law.")
 
-class ManPageFormatter(optparse.HelpFormatter):
-    
+parser = None
+
+class ManPageFormatter(lssh.LongHelpFormatter):
+
     def __init__(self, indent_increment=2, max_help_position=24,
                  width=None, short_first=1):
         """Constructor. Unfortunately HelpFormatter is no new-style class."""
-        optparse.HelpFormatter.__init__(self, indent_increment,
+        lssh.LongHelpFormatter.__init__(self, True, indent_increment,
                                         max_help_position, width, short_first)
 
     def _markup(self, txt):
@@ -48,6 +62,8 @@ class ManPageFormatter(optparse.HelpFormatter):
         result = []
         opts = self.option_strings[option]
         result.append('.TP\n.B %s\n' % self._markup(opts))
+        if option.long_help:
+            option.help = option.long_help
         if option.help:
             help_text = '%s\n' % self._markup(self.expand_default(option))
             result.append(help_text)
@@ -55,25 +71,26 @@ class ManPageFormatter(optparse.HelpFormatter):
 
 
 class build_manpage(Command):
-        
+
     description = 'Generate man page.'
-        
+
     user_options = [('output=', 'O', 'output file')]
-        
+
     def initialize_options(self):
-        self.output = "lssh.1"
+        if not os.path.isdir(BUILD_MANDIR):
+            os.makedirs(BUILD_MANDIR)
+        self.output = BUILD_MANPAGE
         self.parser = None
-        
+
     def finalize_options(self):
-        self.parser = lssh.buildParser()
-        self.parser.formatter = ManPageFormatter()
-        self.parser.formatter.set_parser(self.parser)
+        parser.formatter = ManPageFormatter()
+        parser.formatter.set_parser(parser)
         self.announce('Writing man page %s' % self.output)
         self._today = datetime.date.today()
 
     def _markup(self, txt):
         return txt.replace('-', '\\-')
-        
+
     def _write_header(self):
         appname = self.distribution.get_name()
         ret = []
@@ -86,7 +103,7 @@ class build_manpage(Command):
         else:
             name = self._markup(appname)
         ret.append('.SH NAME\n%s\n' % name)
-        synopsis = self.parser.get_usage()
+        synopsis = parser.get_usage()
         if synopsis:
             synopsis = synopsis.replace('%s ' % appname, '')
             ret.append('.SH SYNOPSIS\n.B %s\n%s\n' % (self._markup(appname),
@@ -98,50 +115,48 @@ class build_manpage(Command):
 
     def _write_options(self):
         ret = ['.SH OPTIONS\n']
-        ret.append(self.parser.format_option_help())
+        ret.append(parser.format_option_help())
         return ''.join(ret)
-                    
+
     def _write_footer(self):
         ret = []
         appname = self.distribution.get_name()
         author = '%s <%s>' % (self.distribution.get_author(),
                               self.distribution.get_author_email())
-        ret.append(('.SH AUTHORS\n.B %s\nwas written by %s.\n'
+        ret.append(('.SH AUTHOR\n.B %s\nwas written by %s.\n'
                     % (self._markup(appname), self._markup(author))))
+        ret.append((".SH COPYRIGHT\n" + COPYRIGHT + "\n"))
         homepage = self.distribution.get_url()
         ret.append(('.SH DISTRIBUTION\nThe latest version of %s may '
                     'be downloaded from\n'
                     '.UR %s\n.UE\n'
                     % (self._markup(appname), self._markup(homepage),)))
+        ret.append('.SH "SEE ALSO"\n.BR ssh(1)\n')
         return ''.join(ret)
-    
+
     def run(self):
-        manpage = []                                                           
-        manpage.append(self._write_header())                                   
-        manpage.append(self._write_options())                                  
-        manpage.append(self._write_footer())                                   
-        stream = open(self.output, 'w')                                        
-        stream.write(''.join(manpage))                                         
+        manpage = []
+        manpage.append(self._write_header())
+        manpage.append(self._write_options())
+        manpage.append(self._write_footer())
+        stream = open(self.output, 'w')
+        stream.write(''.join(manpage))
         stream.close()
-            
+
 build.sub_commands.append(('build_manpage', None))
 
 
-LONG_DESC = \
-  ("Lazarus SSH maintains a SSH session and transparently reconnects as " +
-   "network conditions cause SSH to fail. lssh manages screen sessions on the " +
-   "remote machine for reattaching after restarting the SSH connection. This " +
-   "can be used to resume login sessions on remote machines after putting a " +
-   "laptop to sleep, changing networks, getting a new IP address, or simply " +
-   "to keep ssh usable on a connection with packet loss. ")
-
 if __name__=='__main__':
+    parser = lssh.buildParser(False, "lssh")
     setup(name="lssh",
+          version=lssh.version,
           description="resurrecting SSH from the dead",
-          long_description=LONG_DESC,
+          long_description=lssh.LONG_DESC,
           author="Chris Newton",
           author_email="redshodan@gmail.com",
-          license="GNU GPL",
+          license="GNU GPL v2",
           url="http://code.google.com/p/lazarus-ssh/",
-          cmdclass={'build_manpage': build_manpage},          
+          data_files=[('man/man1', [BUILD_MANPAGE])],
+          scripts=['lssh', 'lssh'],
+          cmdclass={'build_manpage': build_manpage},
           )
